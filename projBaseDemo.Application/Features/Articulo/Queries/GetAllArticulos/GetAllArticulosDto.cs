c2692f9b0493621fbc148ebcf27b28ea5cd98d88
jaar2004 <iarsan_@hotmail.com>
2024-08-09 00:23:28 -0600

    add two code files to review push on dev branch

---
+++ projBaseDemo.Application/Features/Articulo/Queries/GetAllArticulos/GetAllArticulosDto.cs
@@ -0,0 +1,24 @@
+﻿// IAR jue 08AGO2024
+
+using projBaseDemo.Application.Common.Mappings;
+
+namespace projBaseDemo.Application.Features.Articulo.Queries.GetAllArticulos
+{
+    public class GetAllArticulosDto : IMapFrom<Domain.Entities.Articulo>
+    {
+        public int ArticuloId { get; set; }
+        public string Itemno { get; set; }
+        public string Descripcion { get; set; }
+
+        // IAR jue 30MAY2024
+        public string IdCategoria { get; set; }
+
+        public string Almacen { get; set; }
+
+        public bool Status { get; set; }
+
+        public bool Vigente { get; set; }
+
+        public int IdUsuario { get; set; }
+    }
+}
---
+++ projBaseDemo.Application/Features/Articulo/Queries/GetAllArticulos/GetAllArticulosQuery.cs
@@ -0,0 +1,35 @@
+﻿// IAR jue 08AGO2024
+
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using projBaseDemo.Application.Interfaces.Repositories;
+using projBaseDemo.Shared;
+
+
+namespace projBaseDemo.Application.Features.Articulo.Queries.GetAllArticulos
+{
+    public record GetAllArticulosQuery : IRequest<Result<List<GetAllArticulosDto>>>;
+
+    internal class GetAllArticulosQueryHandler : IRequestHandler<GetAllArticulosQuery, Result<List<GetAllArticulosDto>>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetAllArticulosQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<List<GetAllArticulosDto>>> Handle(GetAllArticulosQuery query, CancellationToken cancellationToken)
+        {
+            var players = await _unitOfWork.Repository<Domain.Entities.Articulo>().Entities
+                   .ProjectTo<GetAllArticulosDto>(_mapper.ConfigurationProvider)
+                   .ToListAsync(cancellationToken);
+
+            return await Result<List<GetAllArticulosDto>>.SuccessAsync(players);
+        }
+    }
+}
